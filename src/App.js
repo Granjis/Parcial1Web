fcd65c1ce9c849a38d80598c543bd8ac7d4e75d0
Julio Pena <granjajajaja@Julios-MacBook-Pro.local>
2024-09-26 18:03:02 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import MainMenu from  './MainMenu';
+import MainMenu from './MainMenu';
 import Login from './Login';
+import "./Login.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+ 
   return (
     <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/mainMenu" element={<MainMenu />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/mainMenu" element={<MainMenu />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,97 +1,105 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Login.css";
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
-import { Link } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+
 function Login() {
 
-    const [validationStates, setValidationStates] = useState({ emailState: true, passwordState: true });
+    const [validesCampos, setValidesCampos] = useState({ emailState: true, passwordState: true });
+    const [valoresCampos, setValoresCampos] = useState({ email: "", password: "" });
+    const [esValido, setEsValido] = useState(false);
   
-    const [formValues, setFormValues] = useState({ email: "", password: "", favClass: "1" })
-  
-    const [isValid, setIsValid] = useState([false]);
-  
-    const handleEmailChange = ((e) => {
-  
-      setFormValues({ ...formValues, email: e.target.value })
-      if (formValues.email.includes("@") && formValues.email.includes(".")) {
-        setValidationStates({ ...validationStates, emailState: true })
-        console.log("Email  is valid")
-      }
-      else {
-        setValidationStates({ ...validationStates, emailState: false })
-        console.log("Email  is invalid")
-      }
-  
-    });
-  
-    const handlePasswordChange = ((e) => {
-  
-      setFormValues({ ...formValues, password: e.target.value })
-      setIsValid( /^[0-9a-zA-Z]+$/.test(formValues.password));
-  
-      if (formValues.password.length >= 9 && isValid) {
-        setValidationStates({ ...validationStates, passwordState: true })
-      }
-      else {
-        setValidationStates({ ...validationStates, passwordState: false })
-      }
-  
-    });
-  
-    const handleSelectChange = ((e) => {
-      setFormValues({ ...formValues, favClass: e.target.value })
-  
-    });
-  
-  
-  
-    const clickSubmit = (() => {
-  
-      const errors = [];
-  
-      if (formValues.email === "" || formValues.password === "") {
-        alert("Fill all the fields")
-      }
-      else {
-        if (!validationStates.emailState) {
-          errors.push("Email is not valid")
+    const navigate = useNavigate();
+
+    const handleEmailChange = ((entrada) => {
+        setValoresCampos({ ...valoresCampos, email: entrada.target.value })
+        if (entrada.target.value.includes("@") && entrada.target.value.includes(".")) {
+            setValidesCampos({ ...validesCampos, emailState: true })
+        
         }
-        if (!validationStates.passwordState) {
-          errors.push("Password is not valid")
+        else {
+            setValidesCampos({ ...validesCampos, emailState: false })
+
         }
-        if (errors.length > 0) {
-          alert(errors.join("\n"));
-        } else {
-          alert(JSON.stringify(formValues));
-        }
-      }
-  
     });
-  
+
+    const handlePasswordChange = ((entrada) => {
+
+        setValoresCampos({ ...valoresCampos, password: entrada.target.value })
+        setEsValido(/^[0-9a-zA-Z]+$/.test(entrada.target.value));
+        if (entrada.target.value.length >= 8 && esValido) {
+            setValidesCampos({ ...validesCampos, passwordState: true })
+        }
+        else {
+            setValidesCampos({ ...validesCampos, passwordState: false })
+        }
+    });
+
+    const clickLogin = (() => {
+
+        const errors = [];
+        if (valoresCampos.email === "" || valoresCampos.password === "") {
+            alert("Fill all the fields")
+        }
+        else {
+            if (!validesCampos.emailState) {
+                errors.push("Email is not valid")
+            
+            }
+
+            if (!validesCampos.passwordState) {
+                errors.push("Password is not valid")
+            }
+            if (errors.length > 0) {
+                alert(errors.join("\n"));
+            } else {
+                navigate("/mainMenu")
+          
+            }
+        }
+
+    });
+
     return (
-      <div>
-        <h1>Login</h1>
-  
-        <Form>
-          <Form.Group className="mb-6" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} value={formValues.email} className={!validationStates.emailState ? 'is-invalid' : ''} />
-            {!validationStates.emailState && <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>}
-          </Form.Group>
-  
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} value={formValues.password} className={!validationStates.passwordState ? 'is-invalid' : ''} />
-            {!validationStates.passwordState && <Form.Text className="text-muted">Your password should be have numbers and letters and should be at least 9 char long</Form.Text>}
-          </Form.Group>
-          < Link to="/mainMenu/">
-          <Button variant="primary" onClick={clickSubmit}>
-          login
-          </Button>
-          </Link>
-        </Form>
-      </div>
-    );
-  }
-  
-  export default Login; 
+        <div className="login-background-image">
+
+
+
+            <Container className="login-container">
+                <Row>
+                    <h1 className="login-title">  Log in</h1>
+                </Row>
+                <Row>
+                    <Container className='labels-container'>
+
+                        <Form>
+                            <Form.Group className='mb6' controlId='formEmail'>
+                                <Form.Label> Email </Form.Label>
+                                <Form.Control type='email' onChange={handleEmailChange} value={valoresCampos.email} className={!validesCampos.emailState ? 'is-invalid' : ''} />
+                            </Form.Group>
+
+                            <Form.Group className='mb6' controlId='formPassword'>
+                                <Form.Label> Password </Form.Label>
+                                <Form.Control type='password' onChange={handlePasswordChange} value={valoresCampos.password} className={!validesCampos.passwordState ? 'is-invalid' : ''} />
+                            </Form.Group>
+
+                            <Button variant="primary" onClick={clickLogin} className='login-button'>
+                                Log in
+                            </Button>
+
+                        </Form>
+
+
+                    </Container>
+                </Row>
+
+            </Container>
+
+        </div>
+    )
+
+} export default Login;
